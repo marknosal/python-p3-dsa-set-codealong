c6b0f05f6385ff2658c14a5f3bed4fe100247a06
Mark Nosal <mark.c.nosal@gmail.com>
2023-06-28 01:53:58 -0500

    finished code along

--- lib/MySet.py
+++ lib/MySet.py
@@ -1,3 +1,32 @@
 class MySet:
 
-    pass
+    def __init__(self, list = []):
+        self.dictionary = {}
+        for thing in list:
+            self.dictionary[thing] = True
+
+    def has(self, value):
+        return value in self.dictionary
+
+    def add(self, value):
+        self.dictionary[value] = True
+        return self
+
+    def delete(self, value):
+        self.dictionary.pop(value, None)
+        return self
+
+    def size(self):
+        return len(self.dictionary)
+    
+    def clear(self):
+        self.dictionary = {}
+        return self
+    
+    def __str__(self):
+        set_list = []
+        for k, _ in self.dictionary.items():
+            set_list.append(str(k))
+        return f'MySet: {{{",".join(set_list)}}}'
+    
+
